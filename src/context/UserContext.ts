870f72986c070f7342d2df276338dd77a8ebb34c
Isadora Zhang <izzyorsomething@gmail.com>
2026-05-22 18:30:14 -0400

    god help us all

--- src/context/UserContext.ts
+++ src/context/UserContext.ts
@@ -4,7 +4,7 @@ export type UserItem = { // per item that'll be stored in favorites/cart (a movi
     type: 'movie' | 'tv' | 'season';
     id: number;
     title: string;
-    release: number; // year of release
+    release: number; // release date
     imagePath: string | null;
     seasonNumber?: number; // if it's a season ("?" means optional. only for tv)
 }
@@ -17,11 +17,16 @@ export type UserContextType = { // contains all the context
     setFavorite: (item: UserItem) => void; // function description for a function that is made later, takes/returns x and y
     setCart: (item: UserItem) => void; // takes a UserItem but function returns nothing
     setGenrePreferences: (preferences: number[]) => void; // takes array of selected genres' IDs
-    removeFavorite: (item: UserItem) => void;
-    removeCart: (item: UserItem) => void;
+    removeFavorite: (id: number, type: UserItem['type']) => void; // when looking in existing list: need item's id + type because movie/tv can hv same id
+    removeCart: (id: number, type: UserItem['type']) => void;
     isFavorite: (id: number, type: UserItem['type']) => boolean; // UserItem['variableInUserItemWithThisName']
-    isInCart: (id: number, type: UserItem['type']) => boolean; // must have id AND type cuz movies and tv can hv the same id
+    isInCart: (id: number, type: UserItem['type']) => boolean;
 }
+
+const USERNAME_KEY = "username_key";
+const FAVORITES_KEY = "favorites_key";
+const CART_KEY = "cart_key";
+const GENRES_KEY = "genres_key";
 
 export const UserContext = createContext<UserContextType | undefined>(undefined);
 
