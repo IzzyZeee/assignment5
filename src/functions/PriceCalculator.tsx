4fe3f5df7b676d5940c8a1bf43e9cf4334456e1e
Isadora Zhang <izzyorsomething@gmail.com>
2026-05-25 00:52:16 -0400

    slight change in pricecalc

--- src/functions/PriceCalculator.tsx
+++ src/functions/PriceCalculator.tsx
@@ -4,5 +4,5 @@ export const getPrice = (year: number) => {
     let base = 19.99;
     let min = 4.99;
     let price = CURRENT_YEAR - year;
-    return base - price < min ? min : base - price;
+    return base - price < min ? min : (base - price).toFixed(2);
 }
